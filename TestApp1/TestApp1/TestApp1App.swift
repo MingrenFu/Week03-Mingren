2d3c45341834c1882ea63a199682ebb68d544dde
MingrenFu <mf3668@nyu.edu>
2023-02-14 15:47:09 -0500

    Test App1

---
+++ TestApp1/TestApp1/TestApp1App.swift
@@ -0,0 +1,19 @@
+//
+//  TestApp1App.swift
+//  TestApp1
+//
+//  Created by Michael Fu on 2/14/23.
+//
+
+import SwiftUI
+
+@main
+struct TestApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+
